c7424806523123a2cdb51f49be12b9be8a44e9e7
Annie <backgroundsignals+alt@gmail.com>
2025-06-17 12:05:24 -0400

    Setting up video class

---
+++ video.py
@@ -0,0 +1,18 @@
+class Video:
+    def __init__():
+        #create defaults
+        Video.cap = None
+
+        Video.size = 100
+        Video.resizeToHeight = False
+        video.fpsLimit = 12
+
+        Video.mute = False
+        Video.color = True
+        Video.colorReduction = 16
+
+        Video.frames = []
+        Video.frameDiffs = []
+
+        Video.paused = False
+        Video.pauseTime = 0
